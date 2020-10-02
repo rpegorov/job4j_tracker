8b28c3bebf7250071fbe5348750eab750a6765a9
rpegorov <rostislav.egorov@gmail.com>
2020-10-02 11:28:09 +0300

    Tracker - хранилище [#286604]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithOutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                itemsWithOutNull[size] = item;
+                size++;
+                itemsWithOutNull = Arrays.copyOf(itemsWithOutNull, size);
+            }
+        }
+        return itemsWithOutNull;
+    }
+
+    public Item[] finByName(String key) {
+        Item[] itemsFindName = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item.equals(key)) {
+                itemsFindName[size] = item;
+                size++;
+                itemsFindName = Arrays.copyOf(itemsFindName, size);
+            }
+        }
+        return itemsFindName;
     }
 
     public Item findById(int id) {
