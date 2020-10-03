922baa0d919cbc49653ffc183081f73ea41999da
rpegorov <rostislav.egorov@gmail.com>
2020-10-03 12:17:40 +0300

    Метод замены заявки. Tracker.replace [#286601]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,14 +31,27 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int findId = indexOf(id);
+        if (findId != - 1) {
+            item.setId(id);
+            items[findId] = item;
+        }
+        return findId == -1 ? false : true;
     }
 }
