c3c93ee877764df3db6d5352dabc5c9a2a341585
rpegorov <rostislav.egorov@gmail.com>
2020-11-03 14:12:00 +0300

    Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -31,9 +31,9 @@ public class StartUITest {
         Input in = new StubInput(
                 new String[] {"0", "1", "New item name", "1"}
         );
-               UserAction[] actions = {
-                       new ReplaceAction(out),
-                       new Exit(out)
+        UserAction[] actions = {
+                new ReplaceAction(out),
+                new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
@@ -57,7 +57,7 @@ public class StartUITest {
 
     @Test
     public void showAll() {
-        StubOutput out = new StubOutput();
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Item test"));
         Input in = new StubInput(
@@ -72,6 +72,23 @@ public class StartUITest {
                 + "\r\n=== Show all item ==="
                 + "\r\nItem ID: 1 |  Name: Item test"
                 + "\r\nMenu."
+                + "\r\n=== Exit program ==="
+                + "\r\nExit program, good bay!"
+                + "\r\n"));
+    }
+
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is("Menu."
                 + "\r\n=== Exit program ==="
                 + "\r\nExit program, good bay!"
                 + "\r\n"));
