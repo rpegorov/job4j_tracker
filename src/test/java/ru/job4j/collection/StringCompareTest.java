7b5a8c7fca857d0e3947c62f60e06384b564bd91
rpegorov <rostislav.egorov@gmail.com>
2021-02-15 10:42:36 +0300

    3. Компаратор для строк [#286509] дополнительные тесты

--- src/test/java/ru/job4j/collection/StringCompareTest.java
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -9,7 +10,7 @@ import static org.junit.Assert.*;
 public class StringCompareTest {
 
     @Test
-    public void whenStringsAreEqualThenZero () {
+    public void whenStringsAreEqualThenZero() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -26,5 +27,35 @@ public class StringCompareTest {
                 "Petrov"
         );
         assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenEmptyCompare() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "",
+                ""
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void whenO1EmprtyO2NotEpmty() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "",
+                "Ivanov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenO2EmprtyO1NotEpmty() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                ""
+        );
+        assertThat(rst, greaterThan(0));
     }
 }
