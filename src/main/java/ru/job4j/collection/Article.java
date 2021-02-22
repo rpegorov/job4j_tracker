2e05a03cc93bdb0d229fb56613c7da4faeb2635a
rpegorov <rostislav.egorov@gmail.com>
2021-02-22 11:26:46 +0300

    1. Генератор текста -  проходит. Убираем знаки

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        HashSet<String> rsl = new HashSet<>(Arrays.asList(origin.split(" ")));
+        HashSet<String> rsl = new HashSet<>(Arrays.asList(origin.replaceAll("[.,!]", "").split(" ")));
         String[] valueLine = line.split(" ");
         for (String s : valueLine) {
             if (!rsl.contains(s)) {
