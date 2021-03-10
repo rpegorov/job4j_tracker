f3660272922a9685eef525b6456de14c5cf8438b
rpegorov <rostislav.egorov@gmail.com>
2021-03-10 09:19:28 +0300

    1. Фильтрация учеников. [#286379] Убрана переменная, чекстайл

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -5,10 +5,9 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public List<Student>collect(List<Student> students, Predicate<Student>predict) {
-        List<Student> rsl = students.stream()
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
-        return rsl;
     }
 }
