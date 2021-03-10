89f2072c269af3728736bdce0bb1d21ecdf750aa
rpegorov <rostislav.egorov@gmail.com>
2021-03-10 08:57:06 +0300

    0. Stream API [#286378] 0 не положительный и не отрицательный, ноль познал дзен, будь как ноль - будь нейтрален

--- src/main/java/ru/job4j/stream/Filter.java
+++ src/main/java/ru/job4j/stream/Filter.java
@@ -10,7 +10,7 @@ public class Filter {
         ArrayList<Integer> numbs = new ArrayList<>(
                 Arrays.asList(1, 2, 3, 4, -1, -2, -3, -4, 0, 10, -5));
         List<Integer> rsl = numbs.stream().filter(
-                integer -> integer >= 0
+                integer -> integer > 0
         ).collect(Collectors.toList());
         rsl.forEach(System.out::println);
     }
